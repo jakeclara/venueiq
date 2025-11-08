6ea121b967f19bda4c8d8ddbb8393a15a6a0573b
jakeclara <jakeaclara@gmail.com>
2025-11-08 20:00:26 +0700

    Add placeholder Divs for charts on all pages and create components/charts.py

--- src/pages/budget.py
+++ src/pages/budget.py
@@ -1,9 +1,19 @@
 import dash
 from dash import html
+import dash_bootstrap_components as dbc
 
 dash.register_page(__name__)
 
+# define chart divs to hold callback otput
+budget_table = html.Div("Table to display budget similar to P&L", id="budget-table")
+
+
+# define layout with dbc rows and cols, add divs with visualizations to the columns
 layout = html.Div([
     html.H1('This is our Budget page'),
     html.Div('This is our Budget page content.'),
+    
+    dbc.Row(dbc.Col(budget_table),
+            className="mb-2",
+            id="budget-row-1")
 ])
--- src/pages/restaurant/statement.py
+++ src/pages/restaurant/statement.py
@@ -1,9 +1,19 @@
 import dash
 from dash import html
+import dash_bootstrap_components as dbc
 
 dash.register_page(__name__)
 
+# define chart divs to hold callback otput
+restaurant_statement_table = html.Div("Table to display restaurant financials similar to P&L", id="restaurant-statement-table")
+
+
+# define layout with dbc rows and cols, add divs with visualizations to the columns
 layout = html.Div([
     html.H1('This is our Restaurant Statement page'),
-    html.Div('This is our Restaurant Statment page content.'),
+    html.Div('This is our Restaurant Statement page content.'),
+    
+    dbc.Row(dbc.Col(restaurant_statement_table),
+            className="mb-2",
+            id="restaurant-statement-row-1")
 ])
